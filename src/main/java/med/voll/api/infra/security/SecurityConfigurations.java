e277e1263db4b3db9acddef14fa4d2055a35ca74
Paulo Estrela <dt.paulohenrique@gmail.com>
2025-08-13 23:17:16 -0300

    finalizacao do controle de acesso, verificando se o token enviado pelo usuario é valido, em caso positivo, o usuario é considerado como logado e consegue realizar as requisicoes

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package med.voll.api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,6 +11,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 // Indica que esta é uma classe de configuração do Spring
 @Configuration
@@ -18,21 +20,23 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfigurations {
 
+    @Autowired
+    private SecurityFilter securityFilter;
+
     // Define um bean do tipo SecurityFilterChain que contém as regras de segurança da aplicação
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        return http
-                // Desabilita a proteção contra CSRF (Cross-Site Request Forgery)
-                // Em APIs REST stateless, geralmente não é necessário usar CSRF
-                .csrf(csrf -> csrf.disable())
-
-                // Define que a aplicação não manterá sessões de usuário no servidor (stateless)
-                // Cada requisição deve conter as credenciais/autenticação necessárias
-                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-
-                // Constrói e retorna a cadeia de filtros de segurança configurada
-                .build();
+        return
+                http.csrf(csrf -> csrf.disable())
+                        .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        .authorizeHttpRequests(req -> {
+                            //permite requisições para a URL /login mesmo que o usuário nao esteja logado
+                            req.requestMatchers("/login").permitAll();
+                            //para as demais requisições o usuario deve estar logado
+                            req.anyRequest().authenticated();
+                        })
+                        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                        .build();
     }
 
     @Bean // Indica que o metodo produz um bean que será gerenciado pelo Spring e disponível para injeção
--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -4,7 +4,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.domain.usuario.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -18,25 +21,33 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     private TokenService tokenService;
 
+    @Autowired
+    private UsuarioRepository repository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
         var tokenJWT = recuperarToken(request);
 
-        var subject = tokenService.getSubject(tokenJWT);
+        if (tokenJWT != null) {
+            //recupera o token do cabecalho e valida
+            var subject = tokenService.getSubject(tokenJWT);
+            //recupera o nome do usuario no token
+            var usuario = repository.findByLogin(subject);
+            //forca a autenticacao do usuario que possui o token valido
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
 
         //a linha abaixo é necessaria para chamar os proximos filtros e continuar a aplicacao, nao impedindo/bloqueando a execucao
         filterChain.doFilter(request, response);
-
     }
 
     private String recuperarToken(HttpServletRequest request) {
-
         var authorizationHeader = request.getHeader("Authorization");
-
-        if (authorizationHeader == null) {
-            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
         }
-        return authorizationHeader.replace("Bearer ", "");
+
+        return null;
     }
 }
